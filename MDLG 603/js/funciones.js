32f3f3fe9de0ee8b3f892b05f035d271c0e2ec65
Rebecatdiaz <140541032+Rebecatdiaz@users.noreply.github.com>
2023-07-25 11:16:49 -0600

    Add files via upload

---
+++ MDLG 603/js/funciones.js
@@ -0,0 +1,13 @@
+function adivinar(){
+    var respuesta;
+    respuesta=prompt("Es duro y redondo y se mete hasta el fondo,¿Qué es?")
+    
+    //sí la respuesta es correcta
+       if(respuesta=="El anillo"){
+           alert("¡CORRECTO!")  
+    }
+    //Sino
+    else{
+        alert("¡ERROR!")
+    }
+}
